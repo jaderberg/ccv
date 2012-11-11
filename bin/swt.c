d1decbaf83e910ffd018b7bbe513111d36d6fa91
Max Jaderberg <maxjaderberg@gmail.com>
2012-11-11 01:20:52 +0000

    added swt.c

--- bin/swt.c
+++ bin/swt.c
@@ -7,20 +7,57 @@
 
 #include <ccv.h>
 
-int main(int arc, char** argv)
+int main(int argc, char** argv)
 {
+	// process arguments
+	char* image_file = "";
+	char* out_file = "";
+	int dark_to_bright = 1;
+	char ch;
+	while ((ch = getopt(argc, argv, "r")) != EOF)
+		switch(ch) {
+			case 'r':
+				dark_to_bright = 0;
+				break;
+		}
+	argc -= optind;
+	argv += optind;
+	image_file = argv[0];
+	out_file = argv[1];
+
+
 	// read in image
 	ccv_dense_matrix_t* image = 0;
-	ccv_read(argv[1], &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
+	ccv_read(image_file, &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 
 	// create SWT matrix
 	ccv_dense_matrix_t* swt = 0;
 	ccv_swt_param_t params = ccv_swt_default_params;
 
-	params.direction = CCV_BRIGHT_TO_DARK;
+	if (dark_to_bright) {
+		params.direction = CCV_DARK_TO_BRIGHT;
+		fprintf(stdout, "Computing dark to bright SWT\n");
+	}
+	else {
+		params.direction = CCV_BRIGHT_TO_DARK;
+		fprintf(stdout, "Computing bright to dark SWT\n");
+	}
 
 	ccv_swt(image, &swt, CCV_8U, params);
+	ccv_matrix_free(image);
 
-	ccv_write(swt, argv[2], 0, CCV_IO_JPEG_FILE, 0);
+	// put in visual range
+	ccv_dense_matrix_t* vis_swt = 0;
+	ccv_visualize(swt, &vis_swt, 0);
+	ccv_matrix_free(swt);
+	
+	char* suffix = strrchr(out_file, '.');
+	if (strncmp(suffix, ".png", 4) == 0) {
+		ccv_write(vis_swt, out_file, 0, CCV_IO_PNG_FILE, 0);
+	} else {
+		ccv_write(vis_swt, out_file, 0, CCV_IO_JPEG_FILE, 0);
+	}
+	ccv_matrix_free(vis_swt);
+
     return 0;
 }
